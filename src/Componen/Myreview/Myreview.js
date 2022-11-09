0b268995cd8f3beaf60bda21151666d8989e6369
didipta <66532706+didipta@users.noreply.github.com>
2022-11-09 22:31:14 +0600

    my review add and delete update

--- src/Componen/Myreview/Myreview.js
+++ src/Componen/Myreview/Myreview.js
@@ -5,6 +5,7 @@ import toast, { Toaster } from 'react-hot-toast';
 const Myreview = () => {
     const {user}=useContext(AuthContext);
     const[review,setReview]=useState([]);
+    const[deleteid,setDeleteid]=useState();
     useTitle("My All Review");
     useEffect(()=>{
         fetch(`https://server-side-beta.vercel.app/reviewsbyemail?email=${user.email}`)
@@ -48,9 +49,10 @@ const Myreview = () => {
     <tbody>
         {
             review.map(rev=>
+                <>
                 <tr key={rev._id}>
         <th>
-        <label for="my-modal-6" className="btn">
+        <label for="my-modal-6" className="cursor-pointer" onClick={()=>setDeleteid(rev._id)}>
           <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
           </label>
         </th>
@@ -76,19 +78,22 @@ const Myreview = () => {
         <th>
           <button className="btn btn-ghost btn-xs">Edit</button>
         </th>
-        <div>
+      
+      </tr>
+                  <div>
         <input type="checkbox" id="my-modal-6" className="modal-toggle" />
         <div className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
             <h3 className="font-bold text-lg">Are You sure deleted this review!!</h3>
             <div className="modal-action">
-            <label for="my-modal-6" className="btn btn-danger" onClick={()=>handelDelete(rev._id)}>Yes</label>
+            <label for="my-modal-6" className="btn btn-danger" onClick={()=>handelDelete(deleteid)}>Yes</label>
             <label for="my-modal-6" className="btn btn-success">No</label>
             </div>
         </div>
         </div>
         </div>
-      </tr>
+                </>
+                
       
                 )
         }
